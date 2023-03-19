bb7f7143b42046a0707a8f0dda082881de386c21
gtopsis <topsis@csd.uoc.gr>
2023-03-19 18:40:47 +0200

    Preserve url when user tries to view an unknown thread

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import ThreadView from '@/views/ThreadView.vue'
 import NotFoundView from '@/views/NotFoundView.vue'
+import appData from '@/data.json'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -15,9 +16,24 @@ const router = createRouter({
       path: '/threads/:id',
       name: 'threadShow',
       props: true,
-      component: ThreadView
+      component: ThreadView,
+      beforeEnter(to, from, next) {
+        const threads = appData.threads
+        if (threads.find((t) => t.id === to.params.id)) {
+          return next()
+        }
+        next({
+          name: 'notFound',
+          params: {
+            pathMatch: to.path.substring(1).split('/')
+          },
+          // preserve existing query and hash if any
+          query: to.query,
+          hash: to.hash
+        })
+      }
     },
-    { path: '/:pathMatch(.*)*', name: 'NotFound', component: NotFoundView }
+    { path: '/:pathMatch(.*)*', name: 'notFound', component: NotFoundView }
   ]
 })
 
