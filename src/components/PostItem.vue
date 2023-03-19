e4b658b1463efd0b0b2794f422a64429f218b09c
gtopsis <topsis@csd.uoc.gr>
2023-03-19 19:40:27 +0200

    Style the post item

--- src/components/PostItem.vue
+++ src/components/PostItem.vue
@@ -25,24 +25,31 @@ function getUserById(userId: string | undefined) {
 </script>
 
 <template>
-  <v-card class="post mx-auto mb-2" color="#26c6da" theme="dark" max-width="800">
-    <template v-slot:prepend>
-      <v-icon size="x-large"></v-icon>
-    </template>
-
-    <v-card-text class="post-text line-clamp text-h6 py-2">
-      {{ post.text }}
+  <v-card
+    class="post mx-auto mb-2"
+    loading
+    color="#26c6da"
+    variant="tonal"
+    theme="dark"
+    max-width="1000"
+  >
+    <v-card-text class="post-text py-2">
+      <span class="text-black">{{ post.text }}</span>
     </v-card-text>
 
     <v-card-actions>
-      <v-list-item class="w-100">
+      <v-list-item class="w-100 pa-0">
         <template v-slot:prepend>
           <v-avatar color="grey-darken-3" :image="getUserById(post?.userId)?.avatar"></v-avatar>
         </template>
 
-        <v-list-item-title>{{ getUserById(post?.userId)?.name }}</v-list-item-title>
+        <v-list-item-title class="text-orange">{{
+          getUserById(post?.userId)?.name
+        }}</v-list-item-title>
 
-        <v-list-item-subtitle>{{ getUserById(post?.userId)?.username }}</v-list-item-subtitle>
+        <v-list-item-subtitle class="text-orange-lighten-1">{{
+          getUserById(post?.userId)?.username
+        }}</v-list-item-subtitle>
 
         <!-- <template v-slot:append>
                   <div class="justify-self-end">
@@ -59,12 +66,6 @@ function getUserById(userId: string | undefined) {
 </template>
 
 <style scoped>
-.line-clamp {
-  display: -webkit-box;
-  -webkit-line-clamp: 3;
-  -webkit-box-orient: vertical;
-  overflow: hidden;
-}
 .post {
   width: 100%;
 }
