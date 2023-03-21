2a570f73abfad89917348fce81644dd6759b991c
gtopsis <topsis@csd.uoc.gr>
2023-03-21 16:38:18 +0200

    Move new post form to a modal

---
+++ src/components/PostEditorModal.vue
@@ -0,0 +1,61 @@
+<script setup lang="ts">
+import { ref, computed, toRef } from 'vue'
+import type { Post } from '@/interfaces'
+
+let newPostText = ref('')
+let isNewPostTextEmpty = computed(() => !newPostText.value.trim())
+const props = defineProps<{ dialogEnabled: boolean }>()
+const emits = defineEmits<{ (e: 'save-post', post: Post): void; (e: 'cancel'): void }>()
+
+const dialog = toRef(props, 'dialogEnabled')
+
+const save = () => {
+  if (isNewPostTextEmpty.value) return
+  const post = {
+    edited: {
+      at: 1,
+      by: 'string',
+      moderated: false
+    },
+    publishedAt: Math.floor(Date.now() / 1000),
+    text: newPostText.value,
+    id: 'string',
+    reactions: undefined
+  }
+
+  emits('save-post', post)
+
+  newPostText.value = ''
+}
+
+const cancel = () => {
+  emits('cancel')
+}
+</script>
+
+<template>
+  <v-dialog v-model="dialog" persistent width="600">
+    <v-card>
+      <v-card-title>
+        <span class="text-h5">New post</span>
+      </v-card-title>
+      <v-card-text>
+        <v-container>
+          <v-row>
+            <v-textarea v-model="newPostText" required></v-textarea>
+          </v-row>
+        </v-container>
+        <small>*indicates required field</small>
+      </v-card-text>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn color="blue-darken-1" variant="text" @click="cancel"> Close </v-btn>
+        <v-btn color="blue-darken-1" variant="text" :disabled="isNewPostTextEmpty" @click="save">
+          Save
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<style scoped></style>
--- src/views/ThreadView.vue
+++ src/views/ThreadView.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
 import appData from '@/data.json'
-import { ref, computed } from 'vue'
+import { ref } from 'vue'
 import PostsListing from '@/components/PostsListing.vue'
+import PostEditorModal from '@/components/PostEditorModal.vue'
 
 const props = defineProps<{
   id: string
@@ -9,11 +10,9 @@ const props = defineProps<{
 
 const threads = ref(appData.threads)
 const posts = ref(appData.posts)
-const dialog = ref(false)
-let newPostText = ref('')
+let dialog = ref(false)
 const thread = threads.value.find((t) => t.id === props.id)
 
-let isNewPostTextEmpty = computed(() => !newPostText.value.trim())
 const breadcumbItems = [
   {
     title: 'Threads',
@@ -27,24 +26,15 @@ const getPostsByThreadId = (threadId: string | undefined) => {
   return posts.value.filter((p) => p.threadId === threadId)
 }
 
-const createNewPost = () => {
-  if (isNewPostTextEmpty.value) return
-
-  posts.value.push({
-    edited: {
-      at: 1,
-      by: 'string',
-      moderated: false
-    },
-    publishedAt: Math.floor(Date.now() / 1000),
-    text: newPostText.value,
-    threadId: thread?.id!,
-    userId: thread?.userId!,
-    id: 'string',
-    reactions: undefined
-  })
-  newPostText.value = ''
+const createNewPost = (post) => {
+  post.threadId = thread?.id
+  post.userId = 'random user id'
+  posts.value.push(post)
   dialog.value = false
+}
+
+const openModal = () => {
+  dialog.value = true
 }
 </script>
 
@@ -62,41 +52,18 @@ const createNewPost = () => {
 
     <v-row class="my-2">
       <v-col>
-        <v-btn color="primary" @click="dialog = !dialog"> Create new post </v-btn>
+        <v-btn color="primary" @click="openModal"> Create new post </v-btn>
       </v-col>
     </v-row>
 
-    <v-row justify="center">
-      <v-dialog v-model="dialog" persistent width="600">
-        <v-card>
-          <v-card-title>
-            <span class="text-h5">New post</span>
-          </v-card-title>
-          <v-card-text>
-            <v-container>
-              <v-row>
-                <v-textarea v-model="newPostText" required></v-textarea>
-              </v-row>
-            </v-container>
-            <small>*indicates required field</small>
-          </v-card-text>
-          <v-card-actions>
-            <v-spacer></v-spacer>
-            <v-btn color="blue-darken-1" variant="text" @click="dialog = false"> Close </v-btn>
-            <v-btn
-              color="blue-darken-1"
-              variant="text"
-              :disabled="isNewPostTextEmpty"
-              @click="createNewPost"
-            >
-              Save
-            </v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-dialog>
-    </v-row>
+    <v-row justify="center"> </v-row>
 
     <PostsListing :posts="getPostsByThreadId(thread?.id)"></PostsListing>
+    <PostEditorModal
+      :dialogEnabled="dialog"
+      @save-post="createNewPost"
+      @cancel="dialog = false"
+    ></PostEditorModal>
   </v-container>
 </template>
 
