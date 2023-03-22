d9501965aba35f0b591f8a873d1c84e0c5ac7512
gtopsis <topsis@csd.uoc.gr>
2023-03-22 10:09:59 +0200

    Convert Date comp. into a base one

---
+++ src/components/BaseDateAgo.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+import * as dayjs from 'dayjs'
+import * as relativeTime from 'dayjs/plugin/relativeTime'
+dayjs.extend(relativeTime)
+
+const props = defineProps<{ timestamp: number }>()
+const dateAgo = computed(() => dayjs(props.timestamp).fromNow())
+</script>
+
+<template>
+  <span>{{ dateAgo }}</span>
+</template>
+<style scoped></style>
